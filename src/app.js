3511b68ec4d26e1b8c00f8fb281d09e8f433d98e
menobass <menoecua@gmail.com>
2026-05-25 05:28:47 +0000

    memory leak tweaks

--- src/app.js
+++ src/app.js
@@ -29,8 +29,8 @@ const logger = winston.createLogger({
     winston.format.json()
   ),
   transports: [
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/combined.log' }),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error', maxsize: 20 * 1024 * 1024, maxFiles: 5, tailable: true }),
+    new winston.transports.File({ filename: 'logs/combined.log', maxsize: 50 * 1024 * 1024, maxFiles: 5, tailable: true }),
     new winston.transports.Console({
       format: winston.format.simple()
     })
